928c122ef8dce9b88d8794c0fa1a11e6256e2f7b
rikaehler <rikaehler@outlook.de>
2024-02-07 23:42:38 +0100

    book report on word count and letter count

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 def main():
     path_for_frankenstein = "books/frankenstein.txt"
     text = get_text(path_for_frankenstein)
-
     #print(text) // prints the entire document
     
+    print()
+    print(" - - - This is a report of books/frankenstein.txt - - -")
     word_count = get_number_of_words(text)
     print(f"{word_count} words found in the text")
+    print()
 
 
-    letter_count = get_number_of_letters(text)
-    print(f"these are the numbers letters found in the text: {letter_count}")
-
+    letter_dict = get_number_of_letters(text)
+    #print(f"these are the numbers of letters found in the text: {letter_dict}")
+    
+    list_of_dicts = sort_on(letter_dict)
+    for dict_ in list_of_dicts:
+        for key, value in dict_.items():
+            print(f"The letter '{key}' was found {value} times")
+    
+    print ()
+    print ("- - - End report - - -")
 
 
 # returns text from file as string
@@ -34,6 +43,11 @@ def get_number_of_letters(book):
             result[letter] = lowered_string.count(letter)
     return result
 
-
-          
+def sort_on(unsorted_dict):
+    # sort dict with lamba function in reverse (descending) order  
+    sorted_dict = dict(sorted(unsorted_dict.items(), key=lambda item: item[1], reverse=True))
+    # create alist, add each key:value from sorted_dict into list of dictionaries 
+    lst = [{letter: value for letter, value in sorted_dict.items()}]
+    return lst
+         
 main()
